fd24bfea444c7d28c56be1e98a10c7a2c2deb702
Dustin Tierney <dustintierney@rockethomes.com>
2022-08-03 15:33:56 -0400

    added questions and answer logic to index.js

--- index.js
+++ index.js
@@ -1,31 +1,140 @@
 const Inquirer = require("Inquirer");
-const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-const employee = new Employee();
+const teamMembers = [];
 
-function getManagerInfo() {
-    Inquirer.prompt([
+const managerInfo = () => {
+    return Inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Employee Name:",
+            message: "Name of the team manager:",
         },
         {
             type: "input",
             name: "id",
-            message: "Employee Id:",
+            message: "Manager's employee Id:",
         },
         {
             type: "input",
             name: "email",
-            message: "Employee Email:",
+            message: "Manager's email address:",
         },
         {
             type: "input",
             name: "office",
-            message: "Office Number:",
+            message: "Manager's office number:",
+        },
+        {
+            type: "list",
+            name: "additionalTeamMember",
+            message: "Add another team member?",
+            choices: ['Add Engineer', 'Add Intern', 'Done'],
         },
     ]).then((answers) => {
-        const manager = Manager(answers);
+        const manager = Manager(answers.name, answers.id, answers.email, answers.office)
+        teamMembers.push(manager);
+        switch(answers.additionalTeamMember) {
+            case 'Add Engineer':
+                engineerInfo()
+                break;
+            case 'Add Intern':
+                internInfo()
+                break;
+            default:
+                //
+        }
     });
 }
+
+const engineerInfo = () => {
+    return Inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Name of the Engineer:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Engineer's employee Id:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Engineer's email address:",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Engineer's GitHub username:",
+        },
+        {
+            type: "list",
+            name: "additionalTeamMember",
+            message: "Add another team member?",
+            choices: ['Add Engineer', 'Add Intern', 'Done'],
+        },
+    ]).then((answers) => {
+        const engineer = Engineer(answers.name, answers.id, answers.email, answers.github)
+        teamMembers.push(engineer);
+        switch(answers.additionalTeamMember) {
+            case 'Add Engineer':
+                engineerInfo()
+                break;
+            case 'Add Intern':
+                internInfo()
+                break;
+            default:
+                //
+        }
+    });
+}
+
+const internInfo = () => {
+    return Inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Name of the Intern:",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Intern's employee Id:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern's email address:",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's school:",
+        },
+        {
+            type: "list",
+            name: "additionalTeamMember",
+            message: "Add another team member?",
+            choices: ['Add Engineer', 'Add Intern', 'Done'],
+        },
+    ]).then((answers) => {
+        const intern = Intern(answers.name, answers.id, answers.email, answers.school)
+        teamMembers.push(intern);
+        switch(answers.additionalTeamMember) {
+            case 'Add Engineer':
+                engineerInfo()
+                break;
+            case 'Add Intern':
+                internInfo()
+                break;
+            default:
+                //
+        }
+    });
+}
+
+managerInfo();
